443199a018d90094d1796da2f32ecead5979e0ba
Kribean <caraibe.simulation@gmail.com>
2024-03-09 11:36:38 +0100

    feat: add context

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let tableEvent = document.getElementById("idTable");
 function fetchDataFromApi() {
   fetch(
-    "https://sheets.googleapis.com/v4/spreadsheets/1AbQp861sBhgmRLrN7EmkTvDM0WP7Ub_tJQSSTjT6q2E/values/Activite!A:E?key=AIzaSyCiv16RAu7ZHd8gMmdjtLqbqA9NvjY1YzI"
+    "https://sheets.googleapis.com/v4/spreadsheets/1AbQp861sBhgmRLrN7EmkTvDM0WP7Ub_tJQSSTjT6q2E/values/Activite!A2:F?key=AIzaSyCiv16RAu7ZHd8gMmdjtLqbqA9NvjY1YzI"
   )
     .then((response) => {
       if (!response.ok) {
@@ -13,7 +13,16 @@ function fetchDataFromApi() {
       // Traitement des données
       const eventTab = data.values;
       eventTab.forEach((element) => {
-        let eventElement = `            <div class=" w-full h-fit bg-base-100 shadow-xl p-4 rounded-box my-[10px] text-[#11363f]">
+        let buttonEvent = "";
+        if (element[5]) {
+          buttonEvent = `<div class="w-full lg:w-1/3">
+          <a href="${element[5]}" class="btn btn-info">Cliquer</a>
+        </div>`;
+        }
+        let eventElement = `            <div class=" w-full h-fit bg-base-100 shadow-xl p-4 rounded-box my-[10px] text-[#11363f]" >
+        <div class= "w-full h-[72px]" style="background-image: url(../assets/logo/logo-horizontal-jaune.png); background-repeat:50%; background-size:cover;background-position:center">
+        
+        </div>
         <div class="flex flex-col lg:flex-row justify-between">
         <div class="w-full lg:w-1/3">
           <p class="break-words font-bold">${element[0]}</p>
@@ -22,14 +31,15 @@ function fetchDataFromApi() {
         <p class="break-words">${element[1]}</p>
       </div>
         <div class="w-full lg:w-1/3">
-          <p class="break-words">${element[2]}</p>
+          <p class="break-words font-bold">${element[2]}</p>
         </div>
-        <div class="w-full lg:w-1/3">
+        <div class="w-full lg:w-2/3">
         <p class="break-words">${element[3]}</p>
       </div>
       <div class="w-full lg:w-1/3">
-      <p class="break-words">${element[4]}</p>
+      <p class="break-words font-bold">${element[4]}</p>
     </div>
+    ${buttonEvent}
         </div>
           </div>`;
         tableEvent.innerHTML += eventElement;
