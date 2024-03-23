d991fc56b5280f0c7f80c67d32c1f51d625308d8
Kribean <caraibe.simulation@gmail.com>
2024-03-23 13:24:24 +0100

    fix: adapt to ui

--- js/index.js
+++ js/index.js
@@ -56,7 +56,7 @@ function fetchDataFromApi() {
               </div>`;
         }
         let eventElement = `            <div class=" w-full h-fit bg-base-100 shadow-xl p-4 rounded-box my-[10px] text-[#11363f] dark:bg-base-100 dark:text-[#11363f]" >
-              <div class="flex flex-col lg:flex-row justify-between">
+              <div class="flex flex-col lg:flex-row justify-between items-center">
               <div class="w-full lg:w-1/3">
                 <p class="break-words font-bold">${element[0]}</p>
               </div>
